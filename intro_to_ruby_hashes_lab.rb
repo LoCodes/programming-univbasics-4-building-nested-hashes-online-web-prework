865776a97290be5a4279c60ff2652f8148a0b650
Loren Cipriano <loren25cipriano@gmail.com>
2020-01-22 05:32:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -57,7 +57,8 @@ def monopoly_with_fourth_tier
       },
       names: {
         reading_railroad: {
-          "mortgage value" => {$100},
+          "mortgage value" => {$100}
+        },
         pennsylvania_railroad: {},
         b_and_o_railroad: {},
         shortline_railroad: {}
