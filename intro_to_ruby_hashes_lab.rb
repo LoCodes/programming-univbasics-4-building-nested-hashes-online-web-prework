f7b9793efeb6e0472eab559a3bfdc26785cbd930
Loren Cipriano <loren25cipriano@gmail.com>
2020-01-22 05:18:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,12 +25,7 @@ def monopoly_with_third_tier
     monopoly = {
       pieces: 4,
     railroads: {
-      names: {
-        reading_railroad: {},
-        pennsylvania_railroad: {},
-        b_and_o_railroad: {},
-        shortline_railroad: {}
-        },
+
       rent_in_dollars: {
         one_piece_owned: 25,
         two_pieces_owned: 50,
